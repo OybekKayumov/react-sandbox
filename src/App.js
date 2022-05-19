3d140c1fd28ef1e3ab8258f719d087d9e2376b6a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-19 19:18:53 +0500

    add components/useMemo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import UseRefExample1 from './components/UseRefExample1';
 import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
+import UseMemoExample from './components/UseMemoExample';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="container mt-5" >
       <UseRefExample1 />
       <UseRefExample2 />
-      <UseRefExample3 />      
+      <UseRefExample3 />
+      <UseMemoExample />      
     </div>
   );
 }
---
+++ src/components/UseMemoExample.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
+
+function UseMemoExample() {
+  const [number, setNumber] = useState(1)
+  const [increment, setIncrement] = useState(0)
+
+  // const sqrt = getSqrt(number)
+  // ReRender button isn't in for loo[]
+  const sqrt = useMemo(() => getSqrt(number), [number])
+
+  const renders = useRef(1)
+
+  useEffect(() => {
+    renders.current = renders.current + 1
+  }, [])
+
+  const onClick = () => {
+    setIncrement((prevState) => {
+      console.log(prevState + 1);
+      return prevState + 1
+    })
+  }
+
+  return (
+    <div>
+      <h5>UseMemoExample</h5>
+      <input 
+        type="number" 
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        className="form-control w-25"  
+      />
+
+      <h2 className='my-3'>
+        The sqrt of {number} is {sqrt}
+      </h2>
+
+      <button onClick={onClick} className='btn btn-success mt-2'>Re Render</button>
+      <h3>Renders (from Memo): {renders.current}</h3>
+    </div>
+  )
+}
+
+function getSqrt(n) {
+  for (let i=0; i<=10000; i++) {
+    console.log(i);
+  }
+
+  console.log('Expensive Function Called');
+  return Math.sqrt(n)
+}
+
+export default UseMemoExample
