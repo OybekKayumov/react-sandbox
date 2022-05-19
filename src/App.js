fe93f47cd168d770cf782605c80644ce66ed66fe
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-19 17:41:10 +0500

    add components/useRef2 render and prevName

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UseRefExample1 from './components/UseRefExample1';
+import UseRefExample2 from './components/UseRefExample2';
 
 function App() {
   return (
     <div className="container mt-5" >
       <UseRefExample1 />
+      <UseRefExample2 />
       
     </div>
   );
---
+++ src/components/UseRefExample2.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+
+
+function UseRefExample2() {
+  const [name, setName] = useState('')
+
+  const renders = useRef(1)
+  const prevName = useRef('')
+  
+  useEffect(() => {
+    renders.current = renders.current + 1
+    prevName.current = name
+  }, [name])
+
+  return (
+    <div>
+      <h1>Renders: {renders.current}</h1>
+      <h2>Prev Name state: {prevName.current}</h2>
+      <input 
+        type="text" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="form-control mb-3"  
+      />
+    </div>
+  )
+}
+
+export default UseRefExample2
