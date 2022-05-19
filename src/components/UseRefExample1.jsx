79324e3e9a2478d7287116be2bd6220e414c2f52
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-19 17:17:49 +0500

    add components/useRef inputRef and paraRef

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -3,15 +3,25 @@ import { useRef } from 'react'
 
 function UseRefExample1() {
   const inputRef = useRef()
+  const paraRef = useRef()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputRef.current.value);
+    inputRef.current.value = "Hello"
+    inputRef.current.style.backgroundColor = "orange"
+    paraRef.current.innerText = 'Goodbye'
+  }
 
   return (
     <div>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="" id="name" className='form-control mb-2' />
+        <input type="text" ref={inputRef} name="" id="name" className='form-control mb-2' />
         <button type='submit' className='btn btn-primary'>
           Submit
         </button>
+        <p onClick={() => inputRef.current.focus()} ref={paraRef}>Hello Para</p>
       </form>
     </div>
   )
