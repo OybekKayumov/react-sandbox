1c566eedfdd575b69aea0d5b2d8a57a874adfbc7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-20 12:25:48 +0500

    add hooks/useLocalStorage getLStrgValue

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
 
-function useLocalStorage() {
-  const [localStorageValue, setLocalStorageValue] = useState()
+function useLocalStorage(key, initialValue) {
+  const [localStorageValue, setLocalStorageValue] = useState(() => getLocalStorageValue(key, initialValue))
 
   const setValue = (value) => {
     console.log(value);
   }
 
   return [localStorageValue, setValue]
+}
+
+
+function getLocalStorageValue(key, initialValue) {
+
 }
 
 export default useLocalStorage
