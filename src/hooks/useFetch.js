d033f5d6425f155f7ea7250caad8d355f67e12d2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-20 11:46:46 +0500

    add hooks/useFetch fetch, try, catch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,9 +8,16 @@ function useFetch(url, options) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch()
-      } catch (error) {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        
+        setData(data)
+        setLoading(false)
 
+      } catch (error) {
+        
+        setError(error)
+        setLoading(false)
       }
     }
   })
