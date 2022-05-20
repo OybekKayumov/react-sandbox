9277871ad8db07763be5a3dd6da31d7d23442b1f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-20 12:04:07 +0500

    add components/customHookExample render post.title

--- src/components/CustomHookExample.jsx
+++ src/components/CustomHookExample.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import useFetch from '../hooks/useFetch'
 
 function CustomHookExample() {
-  const response = useFetch('https://jsonplaceholder.typicode.com/posts', {})
-  console.log(response);
+  // const response = useFetch('https://jsonplaceholder.typicode.com/posts', {})
+  // destructuring
+  const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/posts', {})
+  // console.log(response);
+
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
     
   return (
     
     <div>
       <h5 className='mt-4 text-danger'>CustomHookExample</h5>
+      {data.map((post) => (
+        <h3 key={post.id}>{post.title}</h3>
+      ))}
     </div>
   )
 }
